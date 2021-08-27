268b06e8eac4e5f1aeb61ec4eaf3ab74d64b71b4
Stephen <stephenleonardo18@gmail.com>
2021-08-27 12:52:03 +0700

    update styling in navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require('./node_modules/tailwindcss/colors');
 module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				rose: colors.rose,
+				fuchsia: colors.fuchsia,
+				indigo: colors.indigo,
+				teal: colors.teal,
+				lime: colors.lime,
+				orange: colors.orange,
+			},
+		},
 	},
 	variants: {
 		extend: {},

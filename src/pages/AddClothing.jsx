e6aa6d0eca764796e81886d4e27ea65d2473341f
Christian Soetanto <christian.soetanto@binus.edu>
2021-09-01 23:05:17 +0700

    add clothings page

--- src/pages/AddClothing.jsx
+++ src/pages/AddClothing.jsx
@@ -1,61 +1,137 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Card from "../components/UI/Card";
 import useHttp from "../hooks/use-http";
 import { useHistory } from "react-router-dom";
-
+import firebase from "firebase";
+import CustomUploadButton from "react-firebase-file-uploader/lib/CustomUploadButton";
+import SelectTagList from "../components/SelectTag/SelectTagList";
+import CategoryFilter from "../components/Filter/CategoryFilter";
 const AddClothing = (props) => {
 	const history = useHistory();
 
-	const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+	const { isLoading, error, sendRequest } = useHttp();
+	const { isLoading: fetchTags_isLoading, fetchTags_error, sendRequest: fetchTags } = useHttp();
+	const { fetchCategories_isLoading, fetchCategories_error, sendRequest: fetchCategories } = useHttp();
+	const [categories, setCategories] = useState([]);
+	const [selectedCategory, setSelectedCategory] = useState(0);
 
-	const nameInputRef = useRef();
-	const descriptionInputRef = useRef();
-	const categorySelectRef = useRef();
+	const [uploadedFilename, setUploadedFilename] = useState("");
+	const [isUploading, setIsUploading] = useState(false);
+	const [progress, setProgress] = useState(0);
+	const [imageUrl, setImageUrl] = useState("");
+	const [tags, setTags] = useState([]);
 
+	const inputNameRef = useRef();
+
+	const handleUploadStart = () => {
+		setIsUploading(true);
+		setProgress(0);
+	};
+	const handleProgress = (progress) => setProgress(progress);
+	const handleUploadError = (error) => {
+		setIsUploading(false);
+		console.error(error);
+	};
+	const handleUploadSuccess = (filename) => {
+		setUploadedFilename(filename);
+		setProgress(100);
+		setIsUploading(false);
+		firebase
+			.storage()
+			.ref("images")
+			.child(filename)
+			.getDownloadURL()
+			.then((url) => setImageUrl(url));
+	};
+
+	useEffect(() => {
+		fetchTags({ url: "tag/xu7Di7YPp4hvrN250XWwqcy7YVLY" }, (returnData) => {
+			const allTags = [];
+			for (const e of returnData) {
+				allTags.push({ ...e, isSelected: false });
+			}
+			const uniqueTags = [];
+
+			allTags.map((x) => (uniqueTags.filter((a) => a.tagId === x.tagId).length > 0 ? null : uniqueTags.push(x)));
+			setTags(uniqueTags);
+		});
+	}, [fetchTags]);
+
+	useEffect(() => {
+		fetchCategories({ url: "category" }, (returnData) => {
+			setCategories(returnData);
+		});
+	}, [fetchCategories]);
+
+	const selectTagHandler = (tags) => {
+		setTags(tags);
+	};
+	const changedCategoryHandler = (selectedCategory) => {
+		setSelectedCategory(selectedCategory);
+	};
 	const submitHandler = (event) => {
 		event.preventDefault();
-
-		const enteredName = nameInputRef.current.value;
-		const enteredDescription = descriptionInputRef.current.value;
-		const selectedCategory = categorySelectRef.current.value;
+		const enteredName = inputNameRef.current.value;
 
 		const newClothingData = {
 			name: enteredName,
-			category: selectedCategory,
-			description: enteredDescription,
+			imageUrl: imageUrl,
+			tags: tags
+				.filter((t) => t.isSelected)
+				.map((t) => {
+					return {
+						tagId: t.tagId,
+						name: t.name,
+						color: t.color,
+					};
+				}),
+			category: {
+				categoryId: parseInt(selectedCategory),
+			},
 		};
 
 		console.log(newClothingData);
 
+		sendRequest({ url: "clothing/xu7Di7YPp4hvrN250XWwqcy7YVLY", method: "POST", body: newClothingData }, (result) => {
+			console.log(result);
+			history.push("/clothings");
+		});
+
 		//redirect ke clothings di sini
-		history.push("/clothings");
 	};
 
 	return (
 		<Card>
 			<form onSubmit={submitHandler}>
-				<label className='block'>
-					<span className='text-gray-700'>Name</span>
-					<input className='form-input mt-1 block w-full' placeholder='Jane Doe' ref={nameInputRef} />
-				</label>
+				<div className='p-5 m-5 bg-gray-300'>
+					{isUploading && <p>Progress: {progress}</p>}
+					{imageUrl && <img src={imageUrl} />}
+
+					<CustomUploadButton
+						accept='image/*'
+						name='avatar'
+						randomizeFilename
+						storageRef={firebase.storage().ref("images")}
+						onUploadStart={handleUploadStart}
+						onUploadError={handleUploadError}
+						onUploadSuccess={handleUploadSuccess}
+						onProgress={handleProgress}
+						className='bg-gray-300 p-5 m-5'>
+						upload ur amazin clothin
+					</CustomUploadButton>
+				</div>
 
 				<label className='block'>
 					<span className='text-gray-700'>Name</span>
-					<input className='form-textarea mt-1 block w-full' placeholder='' ref={descriptionInputRef} />
+					<input className='form-input mt-1 block w-full' placeholder='' ref={inputNameRef} />
 				</label>
 
-				<label className='block mt-4'>
-					<span className='text-gray-700'>Category</span>
-					<select className='form-select mt-1 block w-full' ref={categorySelectRef}>
-						<option value='1'>cat1</option>
-						<option value='2'>cat2</option>
-						<option value='3'>cat3</option>
-						<option value='4'>cat4</option>
-					</select>
-				</label>
+				<SelectTagList tags={tags} onSelectedTag={selectTagHandler} />
+
+				<CategoryFilter categories={categories} onChangedCategory={changedCategoryHandler} />
 
 				<div>
-					<button>Add Meetup</button>
+					<button>Add Clothings</button>
 				</div>
 			</form>
 		</Card>

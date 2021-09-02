3556f66f6ada6f436e7ef1d8834b266eed6474d4
Stephen <stephenleonardo18@gmail.com>
2021-09-02 13:03:53 +0700

    add validation if not logged in, only show login page

--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -1,10 +1,21 @@
-import MainNavigation from "./MainNavigation";
+import { useContext } from 'react';
+import AuthContext from '../../contexts/auth-context';
+import MainNavigation from './MainNavigation';
+import Login from '../../pages/Login';
 
 function Layout(props) {
+	const ctx = useContext(AuthContext);
+
 	return (
 		<div>
-			<MainNavigation />
-			<main className='p-2'>{props.children}</main>
+			{ctx.isLoggedIn && (
+				<div>
+					<MainNavigation />
+					<main className='p-2'>{props.children}</main>
+				</div>
+			)}
+
+			{!ctx.isLoggedIn && <Login />}
 		</div>
 	);
 }

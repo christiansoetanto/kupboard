e20e9d776a2f64c42a7aa2cf445d1d0bd5c12bd2
Stephen <stephenleonardo18@gmail.com>
2021-09-24 12:07:11 +0700

    refactor calendar

--- src/components/Schedule/GridCalendar.jsx
+++ src/components/Schedule/GridCalendar.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import range from 'lodash/range';
+import DateCell from './DateCell';
 
 const GridCalendar = (props) => {
-	const { dateCells, onDateClick } = props;
+	const { currentMonth, currentYear, onDateClick } = props;
 
 	const daysOfWeek = [
 		'Sunday',
@@ -11,21 +12,67 @@ const GridCalendar = (props) => {
 		'Wednesday',
 		'Thursday',
 		'Friday',
-		'Saturday'
-	]
+		'Saturday',
+	];
 
+
+	const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+		const lastDateOfMonth = new Date(
+			currentYear,
+			currentMonth + 1,
+			0
+		).getDate();
+
+		const tempDateCells = [];
+		let firstDate = 1;
+		for (let index = 1; index <= 35; index++) {
+			if (index >= firstDayOfMonth && firstDate <= lastDateOfMonth) {
+				tempDateCells.push({
+					date: new Date(currentYear, currentMonth, firstDate),
+					key: new Date(currentYear, currentMonth, firstDate),
+					showDate: true,
+				});
+				firstDate++;
+			} else {
+				tempDateCells.push({
+					showDate: false,
+				});
+			}
+		}
+		// setDateCells(tempDateCells);
+		console.log('heheheheh');
+		console.log(tempDateCells);
 
 	return (
 		<div className='flex flex-col gap-y-4'>
 			<div className='grid grid-cols-7'>
-				{daysOfWeek.map(e => {
-					return (<div className={`text-center ${e === 'Sunday' && 'text-red-600'}`}>
-						{e}
-					</div>)
+				{daysOfWeek.map((e) => {
+					return (
+						<div
+							className={`text-center ${
+								e === 'Sunday' && 'text-red-600'
+							}`}
+						>
+							{e}
+						</div>
+					);
 				})}
 			</div>
 			<div className='grid grid-cols-7' onClick={onDateClick}>
-				{dateCells}
+				{tempDateCells.map((e, index) => {
+					if (e.showDate) {
+						return (
+							<DateCell
+								className='flex items-center justify-center border border-gray-300'
+								style={{ minHeight: '6rem' }}
+								date={e.date}
+								key={e.key}
+							/>
+						);
+					} else {
+						return <div key={index + 100}></div>;
+					}
+				})}
 			</div>
 		</div>
 	);
--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -6,39 +6,6 @@ import MonthTitle from './MonthTitle';
 const Schedule = (props) => {
 	const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 	const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-	const [dateCells, setDateCells] = useState(null);
-
-	useEffect(() => {
-
-
-        const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
-        const lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-
-		const tempDateCells = [];
-		let firstDate = 1;
-		for (let index = 1; index <= 35; index++) {
-			if (index >= firstDayOfMonth && firstDate <= lastDateOfMonth) {
-				tempDateCells.push(
-					<DateCell
-						className='flex items-center justify-center border border-gray-300'
-						data-date={index}
-						style={{ minHeight: '6rem' }}
-						date={new Date(currentYear, currentMonth, firstDate)}
-						key={new Date(currentYear, currentMonth, firstDate)}
-					/>
-				);
-				firstDate++;
-			} else {
-				tempDateCells.push(<div key={index + 100}></div>);
-			}
-            setDateCells(tempDateCells);
-		}
-
-        console.log('hahahaha')
-
-	}, [currentMonth, currentYear]);
-
-
 
 	const decreaseMonth = () =>
 		setCurrentMonth((prevState) => (prevState -= 1));
@@ -51,10 +18,14 @@ const Schedule = (props) => {
 			<MonthTitle
 				className='mb-8'
 				currentMonth={currentMonth}
+				currentYear={currentYear}
 				decreaseMonth={decreaseMonth}
 				increaseMonth={increaseMonth}
 			/>
-			<GridCalendar dateCells={dateCells} />
+			<GridCalendar
+				currentMonth={currentMonth}
+				currentYear={currentYear}
+			/>
 		</div>
 	);
 };

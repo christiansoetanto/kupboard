35b5a7ab9a721ee57181784dd9aa1b15d099fe5d
Ivana P <ivana.prawartana@gmail.com>
2021-11-18 21:33:27 +0700

    edit styling profile, text

--- src/components/Layouts/MainNavigation.jsx
+++ src/components/Layouts/MainNavigation.jsx
@@ -17,78 +17,80 @@ function MainNavigation() {
   };
 
   return (
-    <header className="md:px-16 h-16 md:h-24 px-6 bg-white flex flex-wrap items-center py-4 sticky top-0 shadow-sm z-50">
-      <div className="flex flex-1 lg:flex-none items-center mr-6 pb-2">
-        <NavLink to="/" className="font-bold text-2xl">
-          <div className="flex items-center justify-center flex-1">
+    <header className='md:px-16 h-16 md:h-24 px-6 bg-white flex flex-wrap items-center py-4 sticky top-0 shadow-sm z-50'>
+      <div className='flex flex-1 lg:flex-none items-center mr-6 pb-2'>
+        <NavLink to='/' className='font-bold text-2xl'>
+          <div className='flex items-center justify-center flex-1'>
             <img
-              src={require("../../assets/illustrations/logotext2.png").default}
-              alt="logo kupboard"
-              className="lg:h-10 h-8"
+              src={
+                require("../../assets/illustrations/logokupboard.png").default
+              }
+              alt='logo kupboard'
+              className='lg:h-10 h-8'
             />
           </div>
         </NavLink>
       </div>
 
-      <label htmlFor="menu-toggle" className="pointer-cursor lg:hidden block">
+      <label htmlFor='menu-toggle' className='pointer-cursor lg:hidden block'>
         <svg
-          className="fill-current text-gray-900"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
+          className='fill-current text-gray-900'
+          xmlns='http://www.w3.org/2000/svg'
+          width='20'
+          height='20'
+          viewBox='0 0 20 20'
         >
           <title>menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+          <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'></path>
         </svg>
       </label>
-      <input className="hidden" type="checkbox" id="menu-toggle" />
+      <input className='hidden' type='checkbox' id='menu-toggle' />
 
       <div
-        className="hidden lg:flex lg:flex-1 lg:flex-row lg:items-center lg:justify-between lg:w-auto lg:relative absolute top-16 md:top-24 lg:top-0 w-full left-0 px-4 pb-4 lg:p-0 bg-white shadow-md lg:shadow-none"
-        id="menu"
+        className='hidden lg:flex lg:flex-1 lg:flex-row lg:items-center lg:justify-between lg:w-auto lg:relative absolute top-16 md:top-24 lg:top-0 w-full left-0 px-4 pb-4 lg:p-0 bg-white shadow-md lg:shadow-none'
+        id='menu'
       >
-        <nav className="flex">
-          <div className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+        <nav className='flex'>
+          <div className='lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0'>
             <NavLink
-              className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-400"
-              to="/"
+              className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-400'
+              to='/'
               onClick={handleNavLinkOnClick}
             >
               Home
             </NavLink>
             <NavLink
-              className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-purple-400"
-              to="/clothings"
+              className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-purple-400'
+              to='/clothings'
               onClick={handleNavLinkOnClick}
             >
-              Clothings
+              Clothing
             </NavLink>
             <NavLink
-              className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-400"
-              to="/outfits"
+              className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-teal-400'
+              to='/outfits'
               onClick={handleNavLinkOnClick}
             >
-              Outfits
+              Outfit
             </NavLink>
             <NavLink
-              className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
-              to="/schedule"
+              className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400'
+              to='/schedule'
               onClick={handleNavLinkOnClick}
             >
               Schedule
             </NavLink>
             <NavLink
-              className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400"
-              to="/chat"
+              className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400'
+              to='/chat'
               onClick={handleNavLinkOnClick}
             >
               Chat
             </NavLink>
             {ctx.user != null && ctx.user.isAdmin == 1 && (
               <NavLink
-                className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-green-400"
-                to="/approve-request"
+                className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-green-400'
+                to='/approve-request'
                 onClick={handleNavLinkOnClick}
               >
                 Approve Advisor Request
@@ -96,25 +98,25 @@ function MainNavigation() {
             )}
           </div>
         </nav>
-        <nav className="flex">
-          <div className="md:flex md:space-x-3 items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+        <nav className='flex'>
+          <div className='md:flex md:space-x-3 items-center justify-between text-base text-gray-700 pt-4 lg:pt-0'>
             {ctx.user != null && (
               <NavLink
-                className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-orange-400"
-                activeClassName="border-orange-400"
-                to="/profile"
+                className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-orange-400'
+                activeClassName='border-orange-400'
+                to='/profile'
                 onClick={handleNavLinkOnClick}
               >
                 <img
                   src={ctx.user.photoURL}
-                  className="w-10 h-10 rounded-full"
-                  alt=""
+                  className='w-10 h-10 rounded-full'
+                  alt=''
                 />
               </NavLink>
             )}
             <NavLink
-              className="md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-orange-400"
-              to="/"
+              className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-orange-400'
+              to='/'
               onClick={logoutHandler}
             >
               Log Out
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, Fragment, useRef, useState } from "react";
+import React, {
+  useContext,
+  useEffect,
+  Fragment,
+  useRef,
+  useState,
+} from "react";
 import AuthContext from "../../contexts/auth-context";
 import { useHistory } from "react-router-dom";
 import useHttp from "../../hooks/use-http";
@@ -6,247 +12,304 @@ import DatePicker, { Calendar } from "react-multi-date-picker";
 import InputIcon from "react-multi-date-picker/components/input_icon";
 import { confirmAlert } from "react-confirm-alert";
 import CallbackAlert from "../UI/CallbackAlert";
+import "./Profile.css";
+
 const Profile = () => {
-	const history = useHistory();
-	const { isLoading, error, sendRequest } = useHttp();
-	const ctx = useContext(AuthContext);
+  const history = useHistory();
+  const { isLoading, error, sendRequest } = useHttp();
+  const ctx = useContext(AuthContext);
 
-	const [email, setEmail] = useState("");
-	const [isAdvisor, setIsAdvisor] = useState(false);
-	const [gender, setGender] = useState("");
-	const [birthDate, setBirthDate] = useState("");
-	const [advisorRequestStatus, setAdvisorRequestStatus] = useState("");
+  const [email, setEmail] = useState("");
+  const [isAdvisor, setIsAdvisor] = useState(false);
+  const [gender, setGender] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [advisorRequestStatus, setAdvisorRequestStatus] = useState("");
 
-	const nameRef = useRef();
-	const phoneNumberRef = useRef();
-	const instagramRef = useRef();
-	const facebookRef = useRef();
-	const twitterRef = useRef();
-	const linkedInRef = useRef();
-	const otherSocialMediaRef = useRef();
+  const nameRef = useRef();
+  const phoneNumberRef = useRef();
+  const instagramRef = useRef();
+  const facebookRef = useRef();
+  const twitterRef = useRef();
+  const linkedInRef = useRef();
+  const otherSocialMediaRef = useRef();
 
-	const [nameErrorMessage, setNameErrorMessage] = useState("");
-	const [birthDateErrorMessage, setBirthDateErrorMessage] = useState("");
-	const [genderErrorMessage, setGenderErrorMessage] = useState("");
-	const [phoneNumberErrorMessage, setPhoneNumberErrorMessage] = useState("");
+  const [nameErrorMessage, setNameErrorMessage] = useState("");
+  const [birthDateErrorMessage, setBirthDateErrorMessage] = useState("");
+  const [genderErrorMessage, setGenderErrorMessage] = useState("");
+  const [phoneNumberErrorMessage, setPhoneNumberErrorMessage] = useState("");
 
-	const [isCompletedProfile, setIsCompletedProfile] = useState(false);
-	const transformProfile = (returnData) => {
-		const p = returnData.data;
-		setEmail(p.email);
-		setIsAdvisor(p.isAdvisor);
-		nameRef.current.value = p.name;
-		phoneNumberRef.current.value = p.phoneNumber;
-		instagramRef.current.value = p.instagram;
-		setGender(p.gender);
+  const [isCompletedProfile, setIsCompletedProfile] = useState(false);
+  const transformProfile = (returnData) => {
+    const p = returnData.data;
+    setEmail(p.email);
+    setIsAdvisor(p.isAdvisor);
+    nameRef.current.value = p.name;
+    phoneNumberRef.current.value = p.phoneNumber;
+    instagramRef.current.value = p.instagram;
+    setGender(p.gender);
 
-		p.birthDate && setBirthDate(new Date(p.birthDate));
-		facebookRef.current.value = p.facebook;
-		twitterRef.current.value = p.twitter;
-		linkedInRef.current.value = p.linkedIn;
-		otherSocialMediaRef.current.value = p.otherSocialMedia;
-		const required = {
-			birthDate: p.birthDate,
-			gender: p.gender,
-			phoneNumber: p.phoneNumber,
-		};
-		const isAnyEmpty = Object.values(required).some((x) => x === null || x === "");
-		setIsCompletedProfile(!isAnyEmpty);
-	};
+    p.birthDate && setBirthDate(new Date(p.birthDate));
+    facebookRef.current.value = p.facebook;
+    twitterRef.current.value = p.twitter;
+    linkedInRef.current.value = p.linkedIn;
+    otherSocialMediaRef.current.value = p.otherSocialMedia;
+    const required = {
+      birthDate: p.birthDate,
+      gender: p.gender,
+      phoneNumber: p.phoneNumber,
+    };
+    const isAnyEmpty = Object.values(required).some(
+      (x) => x === null || x === ""
+    );
+    setIsCompletedProfile(!isAnyEmpty);
+  };
 
-	useEffect(() => {
-		sendRequest({ url: "user/" + ctx.user.userId + "/" }, (returnData) => {
-			transformProfile(returnData);
-		});
-	}, []);
+  useEffect(() => {
+    sendRequest({ url: "user/" + ctx.user.userId + "/" }, (returnData) => {
+      transformProfile(returnData);
+    });
+  }, []);
 
-	useEffect(() => {
-		sendRequest({ url: "user/advisor/status/" + ctx.user.userId }, (returnData) => {
-			setAdvisorRequestStatus(returnData.status);
-		});
-	}, []);
+  useEffect(() => {
+    sendRequest(
+      { url: "user/advisor/status/" + ctx.user.userId },
+      (returnData) => {
+        setAdvisorRequestStatus(returnData.status);
+      }
+    );
+  }, []);
 
-	const submitProfile = () => {
-		let isError = false;
-		const phoneNumber = phoneNumberRef.current.value;
-		const name = nameRef.current.value;
-		const instagram = instagramRef.current.value;
-		const facebook = facebookRef.current.value;
-		const twitter = twitterRef.current.value;
-		const linkedIn = linkedInRef.current.value;
-		const otherSocialMedia = otherSocialMediaRef.current.value;
+  const submitProfile = () => {
+    let isError = false;
+    const phoneNumber = phoneNumberRef.current.value;
+    const name = nameRef.current.value;
+    const instagram = instagramRef.current.value;
+    const facebook = facebookRef.current.value;
+    const twitter = twitterRef.current.value;
+    const linkedIn = linkedInRef.current.value;
+    const otherSocialMedia = otherSocialMediaRef.current.value;
 
-		if (phoneNumber === "" || phoneNumber === null) {
-			isError = true;
-			setPhoneNumberErrorMessage("Phone Number is required");
-		} else if (phoneNumber.length < 10) {
-			isError = true;
-			setPhoneNumberErrorMessage("Phone Number must be 10 digits or more");
-		} else if (!/^\d+$/.test(phoneNumber)) {
-			isError = true;
-			setPhoneNumberErrorMessage("Phone Number can only contain number");
-		} else {
-			setPhoneNumberErrorMessage("");
-		}
-		if (birthDate === "" || birthDate === null) {
-			isError = true;
-			setBirthDateErrorMessage("Birth Date is required");
-		} else {
-			setBirthDateErrorMessage("");
-		}
-		if (gender === "" || gender === null) {
-			isError = true;
-			setGenderErrorMessage("Gender is required");
-		} else {
-			setGenderErrorMessage("");
-		}
-		if (name === "" || name === null) {
-			isError = true;
-			setNameErrorMessage("Name is required");
-		} else {
-			setNameErrorMessage("");
-		}
-		const data = {
-			name,
-			birthDate,
-			gender,
-			phoneNumber,
-			instagram,
-			facebook,
-			twitter,
-			linkedIn,
-			otherSocialMedia,
-		};
+    if (phoneNumber === "" || phoneNumber === null) {
+      isError = true;
+      setPhoneNumberErrorMessage("Phone Number is required.");
+    } else if (phoneNumber.length < 10) {
+      isError = true;
+      setPhoneNumberErrorMessage("Phone Number must be 10 digits or more.");
+    } else if (!/^\d+$/.test(phoneNumber)) {
+      isError = true;
+      setPhoneNumberErrorMessage("Phone Number can only contain numbers.");
+    } else {
+      setPhoneNumberErrorMessage("");
+    }
+    if (birthDate === "" || birthDate === null) {
+      isError = true;
+      setBirthDateErrorMessage("Birth Date is required.");
+    } else {
+      setBirthDateErrorMessage("");
+    }
+    if (gender === "" || gender === null) {
+      isError = true;
+      setGenderErrorMessage("Gender is required.");
+    } else {
+      setGenderErrorMessage("");
+    }
+    if (name === "" || name === null) {
+      isError = true;
+      setNameErrorMessage("Name is required.");
+    } else {
+      setNameErrorMessage("");
+    }
+    const data = {
+      name,
+      birthDate,
+      gender,
+      phoneNumber,
+      instagram,
+      facebook,
+      twitter,
+      linkedIn,
+      otherSocialMedia,
+    };
 
-		if (!isError)
-			sendRequest({ url: "user/" + ctx.user.userId + "/", method: "PATCH", body: data }, (returnData) => {
-				confirmAlert({
-					customUI: ({ onClose }) => {
-						return <CallbackAlert onClose={onClose} status={"Success"} customMessage={"Profile updated successfully"} />;
-					},
-					afterClose: () => {
-						transformProfile(returnData);
-					},
-				});
-			});
-	};
+    if (!isError)
+      sendRequest(
+        { url: "user/" + ctx.user.userId + "/", method: "PATCH", body: data },
+        (returnData) => {
+          confirmAlert({
+            customUI: ({ onClose }) => {
+              return (
+                <CallbackAlert
+                  onClose={onClose}
+                  status={"Success"}
+                  customMessage={"Profile updated successfully!"}
+                />
+              );
+            },
+            afterClose: () => {
+              transformProfile(returnData);
+            },
+          });
+        }
+      );
+  };
 
-	return (
-		<Fragment>
-			<div>Ini menu profile</div>
+  return (
+    <Fragment>
+      {/* <div>Ini menu profile</div> */}
 
-			<div className=''>
-				<div>PROFILE</div>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Name</span>
-					<input className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg' ref={nameRef} />
-					<span className='text-red-700'>{nameErrorMessage}</span>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Email</span>
-					<input
-						className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg'
-						placeholder=''
-						value={email}
-						disabled={true}
-					/>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Phone Number</span>
-					<input
-						className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg'
-						ref={phoneNumberRef}
-						placeholder='Phone Number'
-						type='number'
-						min='0'
-					/>
-					<span className='text-red-700'>{phoneNumberErrorMessage}</span>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Gender</span>
-					<div>
-						<input type='radio' value='F' name='gender' checked={gender === "F"} onChange={(e) => setGender(e.target.value)} /> Female
-						<input type='radio' value='M' name='gender' checked={gender === "M"} onChange={(e) => setGender(e.target.value)} /> Male
-					</div>
-					<span className='text-red-700'>{genderErrorMessage}</span>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Birth Date</span>
-					<div>
-						<DatePicker
-							value={birthDate}
-							onChange={(e) => {
-								setBirthDate(e?.toDate());
-							}}
-							format='DD MMM YYYY'
-							className='z-0'
-							render={<InputIcon />}
-							maxDate={new Date()}
-						/>
-					</div>
-					<span className='text-red-700'>{birthDateErrorMessage}</span>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Instagram</span>
-					<input
-						className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg'
-						placeholder='Instagram'
-						ref={instagramRef}
-					/>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Facebook</span>
-					<input
-						className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg'
-						placeholder='Facebook'
-						ref={facebookRef}
-					/>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Twitter</span>
-					<input
-						className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg'
-						placeholder='Twitter'
-						ref={twitterRef}
-					/>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>LinkedIn</span>
-					<input
-						className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg'
-						placeholder='LinkedIn'
-						ref={linkedInRef}
-					/>
-				</label>
-				<label className='block w-1/2'>
-					<span className='text-gray-500 text-sm '>Other Social Media</span>
-					<input
-						className=' mt-1 block w-full py-2 focus:outline-none border-0 bg-transparent border-b-2 border-gray-600 text-lg'
-						placeholder='Other Social Media'
-						ref={otherSocialMediaRef}
-					/>
-				</label>
-				<button className='block w-1/2 text-gray-500 text-sm border-collapse bg-blue-400 border-4' onClick={submitProfile}>
-					Submit profile
-				</button>
-			</div>
+      <div className=''>
+        <div className='font-semibold text-3xl text-gray-700 mt-4 mb-4 text-center'>
+          Profile Information
+        </div>
+        <div className='bg-white shadow-xl border p-3 rounded-lg'>
+          <div className='text-xl mb-4'>Profile Picture</div>
+          <div className='text-xl mb-4'>Biodata</div>
 
-			{isAdvisor && <div>kamu sudah jadi advisor</div>}
-			{!isAdvisor && !isCompletedProfile && <div>lengkapi profile untuk request advisor</div>}
-			{!isAdvisor && isCompletedProfile && advisorRequestStatus && advisorRequestStatus !== "Accepted" && (
-				<div>your advisor request is {advisorRequestStatus}</div>
-			)}
-			{!isAdvisor && isCompletedProfile && !advisorRequestStatus && (
-				<button
-					className='bg-blue-300'
-					onClick={() => {
-						history.push(`/request-advisor/`);
-					}}>
-					Request advisor position
-				</button>
-			)}
-		</Fragment>
-	);
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>NAME</span>
+            <input
+              className='block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              ref={nameRef}
+            />
+            <span className='text-red-700 text-sm'>{nameErrorMessage}</span>
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>EMAIL</span>
+            <input
+              className='block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              placeholder=''
+              value={email}
+              disabled={true}
+            />
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>PHONE NUMBER</span>
+            <input
+              className='block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              ref={phoneNumberRef}
+              placeholder='Phone Number'
+              type='number'
+              min='0'
+            />
+            <span className='text-red-700 text-sm'>
+              {phoneNumberErrorMessage}
+            </span>
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>GENDER</span>
+            <div>
+              <input
+                type='radio'
+                value='F'
+                name='gender'
+                checked={gender === "F"}
+                onChange={(e) => setGender(e.target.value)}
+                className=''
+              />{" "}
+              Female
+              <input
+                type='radio'
+                value='M'
+                name='gender'
+                checked={gender === "M"}
+                onChange={(e) => setGender(e.target.value)}
+                className='ml-4'
+              />{" "}
+              Male
+            </div>
+            <span className='text-red-700 text-sm'>{genderErrorMessage}</span>
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>BIRTH DATE</span>
+            <div className=''>
+              <DatePicker
+                value={birthDate}
+                onChange={(e) => {
+                  setBirthDate(e?.toDate());
+                }}
+                format='DD MMM YYYY'
+                className='z-0'
+                render={<InputIcon />}
+                maxDate={new Date()}
+              />
+            </div>
+            <span className='text-red-700 text-sm'>
+              {birthDateErrorMessage}
+            </span>
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>INSTAGRAM</span>
+            <input
+              className=' mt-1 block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              placeholder='Instagram'
+              ref={instagramRef}
+            />
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>FACEBOOK</span>
+            <input
+              className=' mt-1 block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              placeholder='Facebook'
+              ref={facebookRef}
+            />
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>TWITTER</span>
+            <input
+              className=' mt-1 block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              placeholder='Twitter'
+              ref={twitterRef}
+            />
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>LINKEDIN</span>
+            <input
+              className=' mt-1 block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              placeholder='LinkedIn'
+              ref={linkedInRef}
+            />
+          </label>
+          <label className='block w-1/2 mb-3'>
+            <span className='text-gray-500 text-xs '>OTHER SOCIAL MEDIA</span>
+            <input
+              className=' mt-1 block w-full focus:outline-none border-0 bg-transparent border-b-2 border-gray-300'
+              placeholder='Other Social Media'
+              ref={otherSocialMediaRef}
+            />
+          </label>
+          <button
+            className='block w-1/2 my-6 py-2 px-1 hover:bg-purple-400  font-semibold hover:text-white rounded border-2 border-purple-400'
+            onClick={submitProfile}
+          >
+            Update Profile
+          </button>
+          {isAdvisor && <div>You are a Fashion Advisor already.</div>}
+          {!isAdvisor && !isCompletedProfile && (
+            <div>
+              Complete your profile information to request to be a Fashion
+              Advisor!
+            </div>
+          )}
+          {!isAdvisor &&
+            isCompletedProfile &&
+            advisorRequestStatus &&
+            advisorRequestStatus !== "Accepted" && (
+              <div>your advisor request is {advisorRequestStatus}</div>
+            )}
+          {!isAdvisor && isCompletedProfile && !advisorRequestStatus && (
+            <button
+              className='bg-blue-300'
+              onClick={() => {
+                history.push(`/request-advisor/`);
+              }}
+            >
+              Request advisor position
+            </button>
+          )}
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Profile;

095e4c1119896f5a057e0a9c76893bce3695b7d0
Stephen <stephenleonardo18@gmail.com>
2021-11-23 13:23:25 +0700

    update styling in login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from "react";
-import { useHistory } from "react-router-dom";
-import LoginForm from "./LoginForm";
-import AuthContext from "../../contexts/auth-context";
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import AuthContext from '../../contexts/auth-context';
 const Login = () => {
 	const ctx = useContext(AuthContext);
 
@@ -10,23 +10,73 @@ const Login = () => {
 	const loginHandler = async (provider) => {
 		await ctx.onLogin(provider);
 
-		history.push("/");
+		history.push('/');
 	};
 
 	return (
-		<div className='flex flex-col items-center justify-center space-y-6 h-screen bg-cover' style={{minHeight: '100%', backgroundImage: 'url("https://img.rasset.ie/000d8bcc-1600.jpg")'}}>
-			<div className='flex items-center justify-center w-full text-xl md:text-5xl font-semibold h-24' style={{background: 'rgba(120, 113, 108, 0.7)'}}>
-				<div className='px-48 py-8 rounded text-white'>
-					Welcome to <span className='text-amber-300 text-2xl md:text-6xl'>Kupboard</span>
+		<div className='flex flex-col items-center relative px-8 pt-4'>
+			<div className='flex flex-row items-center space-x-2 md:space-x-4'>
+				{/* <div className='text-orange-400 font-bold text-4xl md:text-7xl'> */}
+				<div className='float-left text-orange-400 font-bold text-3xl md:text-7xl'>
+					Welcome to
 				</div>
+				{/* </div> */}
+				{/* <div className=''> */}
+					<img
+						src={
+							require('../../assets/illustrations/logotext_v2.png')
+								.default
+						}
+						alt=''
+						className='float-right md:h-20 h-6'
+						// style={{height: '90px'}}
+					/>
+				{/* </div> */}
 			</div>
-			<div className='flex bg-warmGray-100 w-full flex-col md:flex-row md:space-x-4 space-y-6 md:space-y-0 justify-center items-center w-full md:w-5/6 px-6 py-12 mx-auto'>
-				<div className='flex items-center justify-center flex-1'>
-					<img src={require("../../assets/illustrations/undraw_authentication_fsn5.svg").default} alt='' />
+
+			<div className='flex flex-col md:flex-row w-full md:space-x-4 justify-center md:justify-end items-center'>
+				<div className='flex flex-col space-y-7 shadow-xl rounded-lg border md:w-1/3 px-8 py-4'>
+					<img
+						src={
+							require('../../assets/illustrations/undraw_authentication_fsn5.svg')
+								.default
+						}
+						alt=''
+					/>
+					<div className='text-3xl font-semibold text-center mb-4'>
+						Login With
+					</div>
+
+					<LoginForm loginHandler={loginHandler} />
 				</div>
-				<LoginForm loginHandler={loginHandler} />
+				<div className='md:full'>
+					<div className='-'>
+						<img
+							src={
+								require('../../assets/illustrations/wardrobe.svg')
+									.default
+							}
+							alt=''
+							style={{ mixBlendMode: 'multiply' }}
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
+
+		// <div className='flex flex-col items-center justify-center space-y-6 h-screen bg-cover'>
+		// 	<div className='flex items-center justify-center w-full text-xl md:text-5xl font-semibold h-24' style={{background: 'rgba(120, 113, 108, 0.7)'}}>
+		// 		<div className='px-48 py-8 rounded text-white'>
+		// 			Welcome to <span className='text-amber-300 text-2xl md:text-6xl'>Kupboard</span>
+		// 		</div>
+		// 	</div>
+		// 	<div className='flex bg-warmGray-100 w-full flex-col md:flex-row md:space-x-4 space-y-6 md:space-y-0 justify-center items-center w-full md:w-5/6 px-6 py-12 mx-auto'>
+		// 		<div className='flex items-center justify-center flex-1'>
+		// 			<img src={require("../../assets/illustrations/undraw_authentication_fsn5.svg").default} alt='' />
+		// 		</div>
+		// 		<LoginForm loginHandler={loginHandler} />
+		// 	</div>
+		// </div>
 	);
 };
 
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -4,26 +4,32 @@ import { facebookProvider, googleProvider, twitterProvider } from '../../configs
 const LoginForm = (props) => {
 
 	return (
-		<div className='flex flex-col items-center justify-center space-y-5 flex-1 border-2 px-6 py-12 rounded-md shadow-xl'>
-			<button
-				className='p-2 border-2 border-gray-200 rounded-lg px-6 w-full bg-blue-700 text-gray-100 font-semibold text-right'
-				onClick={() => props.loginHandler(facebookProvider)}
-			>
-				Log in with Facebook
-			</button>
-			<button
-				className='p-2 border-2 border-gray-200 rounded-lg px-6 w-full bg-gray-100 text-gray-900 font-semibold text-right'
-				onClick={() => props.loginHandler(googleProvider)}
-			>
-				Log in with Google
-			</button>
-			<button
-				className='p-2 border-2 border-gray-200 rounded-lg px-6 w-full bg-blue-400 text-gray-200 font-semibold text-right'
-				onClick={() => props.loginHandler(twitterProvider)}
-			>
-				Log in with Twitter
-			</button>
+
+		<div className='flex flex-row justify-evenly'>
+			<div className='cursor-pointer' onClick={() => props.loginHandler(facebookProvider)}><img src={require("../../assets/illustrations/facebook_icon.svg").default} alt="" /></div>
+			<div className='cursor-pointer' onClick={() => props.loginHandler(googleProvider)}><img src={require("../../assets/illustrations/google_icon.svg").default} alt="" /></div>
+			<div className='cursor-pointer' onClick={() => props.loginHandler(twitterProvider)}><img src={require("../../assets/illustrations/twitter_icon.svg").default} alt="" /></div>
 		</div>
+		// <div className='flex flex-col items-center justify-center space-y-5 flex-1 border-2 px-6 py-12 rounded-md shadow-xl'>
+		// 	<button
+		// 		className='p-2 border-2 border-gray-200 rounded-lg px-6 w-full bg-blue-700 text-gray-100 font-semibold text-right'
+		// 		onClick={() => props.loginHandler(facebookProvider)}
+		// 	>
+		// 		Log in with Facebook
+		// 	</button>
+		// 	<button
+		// 		className='p-2 border-2 border-gray-200 rounded-lg px-6 w-full bg-gray-100 text-gray-900 font-semibold text-right'
+		// 		onClick={() => props.loginHandler(googleProvider)}
+		// 	>
+		// 		Log in with Google
+		// 	</button>
+		// 	<button
+		// 		className='p-2 border-2 border-gray-200 rounded-lg px-6 w-full bg-blue-400 text-gray-200 font-semibold text-right'
+		// 		onClick={() => props.loginHandler(twitterProvider)}
+		// 	>
+		// 		Log in with Twitter
+		// 	</button>
+		// </div>
 	);
 };
 

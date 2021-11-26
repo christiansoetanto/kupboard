60376c7e6b10c78b4e5bc41b072a7bf1abc60f90
Christian Soetanto <c.soetantoaa@gmail.com>
2021-11-26 14:56:13 +0700

    add pwa i guess

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './contexts/auth-context';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/auth-context";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -14,8 +15,13 @@ ReactDOM.render(
 			</AuthContextProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
-	document.getElementById('root')	
+	document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
